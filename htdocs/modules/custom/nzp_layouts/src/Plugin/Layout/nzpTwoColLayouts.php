79eda89d4ce9fb0278b9d9c966309466cdcaf9b8
Mohammed El-Khatib <elkhatibm@si.edu>
2019-07-19 17:10:34 -0400

    major updates to the nzp_layouts module and added lullabots  custom_layouts module back into the repo so I can learn from it

--- htdocs/modules/custom/nzp_layouts/src/Plugin/Layout/nzpTwoColLayouts.php
+++ htdocs/modules/custom/nzp_layouts/src/Plugin/Layout/nzpTwoColLayouts.php
@@ -2,47 +2,28 @@
 
 namespace Drupal\nzp_layouts\Plugin\Layout;
 
-use Drupal\Core\Layout\LayoutDefault;
-use Drupal\Core\Form\FormStateInterface;
-use Drupal\Core\Plugin\PluginFormInterface;
-
-
 /**
- * Credit goes to npinos at https://github.com/npinos/drupal8-layouts.
-s */
-class nzpTwoColLayouts extends LayoutDefault implements PluginFormInterface {
-
-
-  /**
-   * {@inheritdoc}
-   */
-  public function __construct(array $configuration, $plugin_id, $plugin_definition) {
-    parent::__construct($configuration, $plugin_id, $plugin_definition);
-  }
+ * Configurable twocol layout plugin class.
+ *
+ * @internal
+ *   Plugin classes are internal.
+ */
+class NzpTwoColLayouts extends NzpWrapperBase {
 
   /**
    * {@inheritdoc}
    */
-  public function defaultConfiguration() {
-    return parent::defaultConfiguration() + [
-      'col-width' => [],
-      'wrapper_class' => '',
-    ];
-  }
 
-  /**
-   * {@inheritdoc}
-   */
-  public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
-    $configuration = $this->getConfiguration();
-    $regions = $this->getPluginDefinition()->getRegions();
-
-    $wrapper_options = [
+  protected function getContainerClasses() {
+    return [
       'container' => 'container',
       'container-fuid' => 'full-width',
     ];
+  }
 
-    $col_options = [
+
+  protected function getRegionClasses() {
+    return [
       'gr' => 'flex',
       'gs1' => '1/12',
       'gs2' => '2/12',
@@ -57,47 +38,14 @@ class nzpTwoColLayouts extends LayoutDefault implements PluginFormInterface {
       'gs11' => '11/12',
       'gs12' => '12/12',
     ];
+  }
 
-
-
-    $form['wrapper_classes'] = [
-      '#group' => 'Wrapper Class',
-      '#type' => 'select',
-      '#options' => $wrapper_options,
-      '#title' => $this->t('Wrapper Class'),
-      '#description' => $this->t('Select a Wrapper Class'),
-      '#default_value' => !empty($configuration['wrapper_class']) ? $configuration['wrapper_class'] : 'container',
+  protected function getHtmlElements() {
+    return [
+      'div' => 'Div',
+      'section' => 'Section',
+      'aside' => 'Aside',
     ];
-
-    foreach ($regions as $region_name => $region_definition) {
-      $form['col_widths_form'][$region_name] = [
-        '#type' => 'select',
-        '#options' => $col_options,
-        '#title' => $this->t('Width for @region', ['@region' => $region_definition['label']]),
-        '#default_value' => !empty($configuration['col_width'][$region_name]) ? $configuration['col_width'][$region_name] : 'gr',
-      ];
-    }
-
-    return $form;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function validateConfigurationForm(array &$form, FormStateInterface $form_state) {
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
-    // $this->configuration['attributes'] = $form_state->getValue('attributes');
-    // foreach (['col_width', 'wrapper_class', 'wrapper_id'] as $name) {
-    //   $this->configuration[$name] = $this->configuration['attributes'][$name];
-    //   unset($this->configuration['attributes'][$name]);
-    // }
-    $this->configuration['col_width'] = $form_state->getValue('col_widths_form');
-    $this->configuration['wrapper_class'] = $form_state->getValue('wrapper_classes');
   }
 
 }
