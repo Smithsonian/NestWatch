905eb2e63cb0b1adbdffc63eeab93e4fb7e98bcd
Mohammed El-Khatib <elkhatibm@si.edu>
2019-07-23 16:57:54 -0400

    going to work on fixing this later

--- htdocs/modules/custom/nzp_layouts/src/Plugin/Layout/nzpTabLayouts.php
+++ htdocs/modules/custom/nzp_layouts/src/Plugin/Layout/nzpTabLayouts.php
@@ -6,7 +6,6 @@ use Drupal\Core\Layout\LayoutDefault;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Plugin\PluginFormInterface;
 
-
 /**
  * Credit goes to npinos at https://github.com/npinos/drupal8-layouts.
 */
@@ -29,8 +28,8 @@ use Drupal\Core\Plugin\PluginFormInterface;
   public function __construct(array $configuration, $plugin_id, $plugin_definition) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
 
-  
-  }
+
+}
 
 
 
@@ -51,33 +50,80 @@ use Drupal\Core\Plugin\PluginFormInterface;
   public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
     $configuration = $this->getConfiguration();
 
-    $form['tab_number'] = [
-        '#type' => 'textfield',
-        '#attributes' => array(
-            ' type' => 'number', // insert space before attribute name :)
-        ),
-        '#title' => 'number of tabs',
-        '#required' => true,
-        '#maxlength' => 3
-    ];
-    $num = $form_state->getValue('tab_number');
-    // $tab_num = filter_var($num, FILTER_SANITIZE_NUMBER_INT);
-    $tab_num = 3;
-    // if (!empty($num)) {
-        for ($i=0; $i < $tab_num; $i++) { 
-            $form['tab_title'][$i] = [
-                '#type' => 'textfield',
-                '#title' => $this->t('Tab Title for Tab:' . $i),
-                '#default_value' => $configuration['tab_title'],
-            ];
+        $i = 0;
+        $tabs_field = $form_state->getValue('num_tabs');
+
+        $form['#tree'] = TRUE;
+        $form['tabs_fieldset'] = [
+          '#type' => 'fieldset',
+          '#title' => $this->t('Number of Tabs'),
+          '#ajax' => TRUE,
+          '#prefix' => '<div id="tabs-fieldset-wrapper">',
+          '#suffix' => '</div>',
+        ];
+        
+        $tabs_field = $form_state->setValue('num_tabs', 2);
+        for ($i = 0; $i < $tabs_field; $i++) {
+          $form['tabs_fieldset']['tab'][$i] = [
+            '#type' => 'textfield',
+            '#title' => t('Tab Label'),
+            '#default_value' => !empty($configuration['tab_title'][$i]) ? $configuration['tab_title'][$i] : 'Tab 1',
+          ];
         }
-    // }
-
-
-
-    return $form;
-}
-
+        $form['actions'] = [
+          '#type' => 'actions',
+        ];
+        $form['tabs_fieldset']['actions']['add_tab'] = [
+          '#type' => 'submit',
+          '#value' => t('Add another Tab'),
+          '#submit' => array('::addOne'),
+          '#ajax' => [
+            'callback' => '::addmoreCallback',
+            'wrapper' => 'tabs-fieldset-wrapper',
+          ],
+        ];
+        if ($tabs_field > 1) {
+          $form['tabs_fieldset']['actions']['remove_tab'] = [
+            '#type' => 'submit',
+            '#value' => t('Remove a Tab'),
+            '#submit' => array('::removeCallback'),
+           '#ajax' => [
+              'callback' => '::addmoreCallback',
+              'wrapper' => 'tabs-fieldset-wrapper',
+            ]
+          ];
+        }
+        $form_state->setCached(FALSE);
+   
+    
+        return $form;
+      }
+    
+      public function getFormId() {
+        return 'tabs_add_more';
+      }
+    
+      public function addOne(array &$form, FormStateInterface $form_state) {
+        $tabs_field = $form_state->get('num_tabs');
+        $add_button = $tabs_field + 1;
+        $form_state->set('num_tabs', $add_button);
+        $form_state->setRebuild();
+      }
+    
+      public function addmoreCallback(array &$form, FormStateInterface $form_state) {
+        $tabs_field = $form_state->get('num_tabs');
+        return $form['tabs_fieldset'];
+      }
+    
+      public function removeCallback(array &$form, FormStateInterface $form_state) {
+        $tabs_field = $form_state->get('num_tabs');
+        if ($tabs_field > 1) {
+          $remove_button = $tabs_field - 1;
+          $form_state->set('num_tabs', $remove_button);
+        }
+       $form_state->setRebuild();
+    }
+      
 
   /**
    * {@inheritdoc}
@@ -89,8 +135,7 @@ use Drupal\Core\Plugin\PluginFormInterface;
    * {@inheritdoc}
    */
   public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
-    
-    $this->configuration['tab_title'] = $form_state->getValue('tab_title');
+    $this->configuration['tab_title'] = $form_state->getValue(array('tabs_fieldset', 'tab'));
     
   }
 
