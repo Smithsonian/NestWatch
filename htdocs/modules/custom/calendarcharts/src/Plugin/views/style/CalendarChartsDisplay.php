614423cf9cd180c89d5b4d9b7da60bc2e875634b
Mohammed El-Khatib <elkhatibm@si.edu>
2019-10-03 16:46:50 -0400

    pushing up updates to the calendarcharts views style plugin

--- htdocs/modules/custom/calendarcharts/src/Plugin/views/style/CalendarChartsDisplay.php
+++ htdocs/modules/custom/calendarcharts/src/Plugin/views/style/CalendarChartsDisplay.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace Drupal\fullcalendar_view\Plugin\views\style;
+namespace Drupal\calendarcharts\Plugin\views\style;
 
+use Drupal\core\form\FormStateInterface;
 use Drupal\field\Entity\FieldStorageConfig;
 use Drupal\views\Plugin\views\style\StylePluginBase;
 use Drupal\Core\Datetime\DrupalDateTime;
@@ -13,13 +14,13 @@ use Drupal\Core\Datetime\DrupalDateTime;
  *
  * @ViewsStyle(
  *   id = "calendarcharts_view_display",
- *   title = @Translation("Calendar Charts Display"),
+ *   title = @Translation("Calendar Charts"),
  *   help = @Translation("Render contents in Calendar Charts view."),
  *   theme = "views_view_calendarcharts",
  *   display_types = { "normal" }
  * )
  */
-class CalendarCharts extends StylePluginBase {
+class CalendarChartsDisplay extends StylePluginBase {
 
   /**
    * {@inheritdoc}
@@ -47,12 +48,6 @@ class CalendarCharts extends StylePluginBase {
    */
   public function buildOptionsForm(&$form, FormStateInterface $form_state) {
     parent::buildOptionsForm($form, $form_state);
-
-    // Remove the grouping setting.
-    if (isset($form['grouping'])) {
-      unset($form['grouping']);
-    }
-
     // All selected fields.
     $field_names = $this->displayHandler->getFieldLabels();
     $entity_type = $this->view->getBaseEntityType()->id();
@@ -100,9 +95,9 @@ class CalendarCharts extends StylePluginBase {
       '#attributes' => [
         ' type' => 'number', // insert space before attribute name :)
       ],
-      
-      '#default_value' => (!empty($this->options['cellColor'])) ? $this->options['cellColor'] : '',
+      '#default_value' => (!empty($this->options['cellSize'])) ? $this->options['cellSize'] : '',
       ];
+
       $form['cellStroke'] = [
         '#type' => 'textfield',
         '#title' => 'Cell Stroke Size',
@@ -110,138 +105,11 @@ class CalendarCharts extends StylePluginBase {
         '#attributes' => [
           ' type' => 'number', // insert space before attribute name :)
         ],
-        
-        '#default_value' => (!empty($this->options['cellColor'])) ? $this->options['cellColor'] : '',
+        '#default_value' => (!empty($this->options['cellStroke'])) ? $this->options['cellSize'] : '',
         ];
-    $form['cellColor'] = [
-      '#type' => 'select',
-      '#title' => 'Select a cell color',
-      '#fieldset' => 'cellOptions',
-      '#options' => $cell_colors, 
-      '#default_value' => (!empty($this->options['cellColor'])) ? $this->options['cellColor'] : '',
-      ];
 
-    if (!isset($form_state->getUserInput()['style_options'])) {
-      // Taxonomy color input boxes.
-      $form['color_taxonomies'] = $this->taxonomyColorService->colorInputBoxs($this->options['vocabularies'], $this->options['color_taxonomies']);
-    }
-    // Content type colors.
-    $form['color_bundle'] = [
-      '#type' => 'details',
-      '#title' => t('Colors for Bundle Types'),
-      '#description' => t('Specify colors for each bundle type. If taxonomy color is specified, this settings would be ignored.'),
-      '#fieldset' => 'colors',
-    ];
-    // All bundle types.
-    $bundles = \Drupal::service('entity_type.bundle.info')->getBundleInfo($entity_type);
-    // Options list.
-    $bundlesList = [];
-    foreach ($bundles as $id => $bundle) {
-      $label = $bundle['label'];
-      $bundlesList[$id] = $label;
-      // Content type colors.
-      $form['color_bundle'][$id] = [
-        '#title' => $label,
-        '#default_value' => isset($this->options['color_bundle'][$id]) ? $this->options['color_bundle'][$id] : '#3a87ad',
-        '#type' => 'color',
-      ];
-    }
-    $moduleHandler = \Drupal::service('module_handler');
-    if ($moduleHandler->moduleExists('calendar_recurring_event')) {
-      // Recurring event.
-      $form['recurring'] = [
-        '#type' => 'details',
-        '#title' => t('Recurring event settings'),
-          // '#description' =>  t('Settings for recurring event.'),.
-      ];
-      // Recurring business start time.
-      $form['business_start'] = [
-        '#type' => 'datetime',
-        '#title' => t('Business start time'),
-        '#description' => t('The time of a day when a recurring all day event starts. The recurring events whose start date include hour and minute will use their respective start time instead.'),
-        '#fieldset' => 'recurring',
-        '#default_value' => empty($this->options['business_start']) ? new DrupalDateTime('2018-02-24T08:00:00') : new DrupalDateTime($this->options['business_start']),
-      // Hide date element.
-        '#date_date_element' => 'none',
-      // You can use text element here as well.
-        '#date_time_element' => 'time',
-        '#date_time_format' => 'H:i',
-      ];
-      // Recurring business end time.
-      $form['business_end'] = [
-        '#type' => 'datetime',
-        '#title' => t('Business end time'),
-        '#description' => t('The time of a day when a recurring event ends. The recurring events whose end date include hour and minute will use their respective end time instead.'),
-        '#fieldset' => 'recurring',
-        '#default_value' => empty($this->options['business_end']) ? new DrupalDateTime('2018-02-24T18:00:00') : new DrupalDateTime($this->options['business_end']),
-      // Hide date element.
-        '#date_date_element' => 'none',
-      // You can use text element here as well.
-        '#date_time_element' => 'time',
-        '#date_time_format' => 'H:i',
-      ];
-    }
-    // New event bundle type.
-    $form['bundle_type'] = [
-      '#title' => $this->t('Event bundle (Content) type'),
-      '#description' => $this->t('The bundle (content) type of a new event. Once this is set, you can create a new event by double clicking a calendar entry.'),
-      '#type' => 'select',
-      '#options' => $bundlesList,
-      '#default_value' => (!empty($this->options['bundle_type'])) ? $this->options['bundle_type'] : '',
-    ];
-    // Extra CSS classes.
-    $form['classes'] = [
-      '#type' => 'textfield',
-      '#title' => t('CSS classes'),
-      '#default_value' => (isset($this->options['classes'])) ? $this->options['classes'] : '',
-      '#description' => t('CSS classes for further customization of this view.'),
-    ];
+
   }
-
-  /**
-   * Options form submit handle function.
-   *
-   * @see \Drupal\views\Plugin\views\PluginBase::submitOptionsForm()
-   */
-  public function submitOptionsForm(&$form, FormStateInterface $form_state) {
-    $options = &$form_state->getValue('style_options');
-    $input_value = $form_state->getUserInput();
-    $input_colors = isset($input_value['style_options']['color_taxonomies']) ? $input_value['style_options']['color_taxonomies'] : [];
-    // Save the input of colors.
-    foreach ($input_colors as $id => $color) {
-      if (!empty($color)) {
-        $options['color_taxonomies'][$id] = $color;
-      }
-    }
-    // Datetime fields in Drupal 8 are stored as strings.
-    if (isset($options['business_start'])) {
-      $options['business_start'] = $options['business_start']->format(DATETIME_DATETIME_STORAGE_FORMAT);
-    }
-    if (isset($options['business_end'])) {
-      $options['business_end'] = $options['business_end']->format(DATETIME_DATETIME_STORAGE_FORMAT);
-    }
-
-    parent::submitOptionsForm($form, $form_state);
-  }
-
-  /**
-   * Taxonomy colors Ajax callback function.
-   */
-  public static function taxonomyColorCallback(array &$form, FormStateInterface $form_state) {
-    $options = $form_state->getValue('style_options');
-    $vid = $options['vocabularies'];
-    $taxonomy_color_service = \Drupal::service('fullcalendar_view.taxonomy_color');
-
-    if (isset($options['color_taxonomies'])) {
-      $defaultValues = $options['color_taxonomies'];
-    }
-    else {
-      $defaultValues = [];
-    }
-    // Taxonomy color boxes.
-    $form['color_taxonomies'] = $taxonomy_color_service->colorInputBoxs($vid, $defaultValues, TRUE);
-
-    return $form['color_taxonomies'];
-  }
-
 }
+
+  
